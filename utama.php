42acac546b0c21a967e0b08eed9d1146447a5a2b
nabila <70184112+nbilasals@users.noreply.github.com>
2022-06-22 14:05:34 +0700

    Add files via upload

---
+++ utama.php
@@ -0,0 +1,235 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+    <!-- CSS Lainnya -->
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/responsive.css">
+
+    <!-- Favicons -->
+    <link href="assets/logo.png" rel="icon">
+
+    <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+
+    <title> Halo, Apen!</title>
+  </head>
+  <body>
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top" id="navbar" style=>
+        <div class="container">
+            <a class="navbar-brand" href="#home">#ForApen</a>
+        </div>
+    </nav>
+
+    <!-- Navbar Ends -->
+
+    <!-- Jumbotron -->
+    <div class="jumbotron bg-white">
+        <div class="container mt-3">
+          <div class="row justify-content-between align-items-center">
+            <div class="col-md-6 col-lg-5 text-center text-md-left mb-5">
+              <h1 class="h1 font-weight-bold">Hey, Apen Junior!</h1>
+              <p class="lead text-secondary my-4">
+            Aku pengen ngomong sesuatu, nih. Boleh, ga?
+              </p>
+
+              <a class="btn btn-danger button" href="maaf.html" role="button"
+                >Boleh, dong.</a
+              >
+            </div>
+            <div class="col-md-6 col-lg-5">
+              <div class="jumbotron_img">
+                <img src="assets/haha.gif" class="img-fluid" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Jumbotron ends -->
+
+      <!-- Features -->
+      <div class="container">
+        <section id=features> 
+          <h2 class="section-title text-center my-3">Here are the reasons why i love Apen Junior</h2>
+          <span class="section-line"></span>
+          <p class="text-center w-75 mx-auto">
+           Kamu selalu nanya kenapa sih aku mau sama kamu, kenapa aku ga jadi ninggalin kamu. Mentang-mentang aku gitu, ya? Ikut-ikutan wleeee. Nih, alesan bila syg apen.
+          </p>
+  
+          <div class="row mt-5 text-center">
+            <div class="col-md-6 col-lg-4">
+              <div class="card features-card">
+                <div class="features-card_img mt-3">
+                  <img src="assets/baikk.png" width="20%" alt="" />
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Apen baik hati</h5>
+                  <p class="card-text">
+                   walaupun apen suka marah-marah di ML sama org random, apen ga pernah marahin bila :(
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+              <div class="card features-card">
+                <div class="features-card_img mt-3">
+                  <img src="assets/lucu.jpg" width="20%"alt="" />
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Apen lucu</h5>
+                  <p class="card-text">
+                    aku selalu ngerasa ga nyambung sama jokes org tp kalo jokes apen cocok! apen jg suka ketawa kan ama jokes aku
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+              <div class="card features-card">
+                <div class="features-card_img mt-3">
+                  <img src="assets/satisfaction.png" width="20%" alt="" />
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Apen sayang bila</h5>
+                  <p class="card-text">
+                    kalo apen tiba tiba bilang, "aku syg bgt tau sm kamu", itu aku maooo mleyot tp berusaha stay cool whuwdkqw
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+              <div class="card features-card">
+                <div class="features-card_img mt-3">
+                  <img src="assets/learn.png" width="20%" alt="" />
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Apen pinter</h5>
+                  <p class="card-text">
+                   jgn seneng dulu, bukan pinter yg itu, tp pinter yg kek pinter, ih maksudnya pinter loh, problem solving gt
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+              <div class="card features-card">
+                <div class="features-card_img mt-3">
+                  <img src="assets/memory.png" width="20%" alt="" />
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Apen selalu ngabarin bila</h5>
+                  <p class="card-text">
+                   aku tau ngabarin tu susah makanya aku makasih bgt apen selalu ngabarin aku, walau kdg aku lagi sibuk tp tetep dikabarin dan dicariin
+                   </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+              <div class="card features-card">
+                <div class="service-card_img mt-3">
+                  <img src="assets/error.png" width="20%" alt="" />
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Apen goblo</h5>
+                  <p class="card-text">
+                    aku tu ilfeel tau sama cowo goblok, tp apen gapapa, haha mungkin itulah kenapa bila syg sm apen krn apa yang aku benci di orang, gapapa kalo di apen
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="text-center my-5">
+          <a class="btn btn-outline-danger button" href="https://alvo.chat/1Nh" role="button"
+                >Mau lebih banyak?</a
+              >
+          </div>
+        </section>
+  <!-- Features ends -->
+
+<!---Testi Ends--->
+        
+<!---FAQ--->
+        <div class="container">
+          <section id="faq" class="faq">
+            <div class="section-title my-3 text-center">
+                <h2>Gombalan #ForApen</h2>
+                <span class="section-line"></span>
+                <p class="text-center my-3"> Apen kapan mau gombalin aku???</p>
+            </div>
+            <div class="accordion my-5" id="accordionExample">
+                <div class="card">
+                  <div class="card-header" id="headingOne">
+                    <h2 class="mb-0">
+                      <button class="btn btn-link btn-block text-left " type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Apen tuh kaya gunting batu kertas.
+                      </button>
+                    </h2>
+                  </div>
+              
+                  <div id="collapseOne" class="collapse show mx-3" aria-labelledby="headingOne" data-parent="#accordionExample">
+                    <div class="card-body">
+                    iya, sweet.
+                    </div>
+                  </div>
+                </div>
+                <div class="card">
+                  <div class="card-header" id="headingTwo">
+                    <h2 class="mb-0">
+                      <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Apen tau ga persamaan apenn sama bola yang ga masuk di gawang?
+                      </button>
+                    </h2>
+                  </div>
+                  <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                    <div class="card-body">
+                      Sayang sekali, pemirsahhhh ^_^
+                    </div>
+                  </div>
+                </div>
+                <div class="card">
+                  <div class="card-header" id="headingThree">
+                    <h2 class="mb-0">
+                      <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Lagu lagu apa yang paling sayang sama apen?
+                      </button>
+                    </h2>
+                  </div>
+                  <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                    <div class="card-body">
+                     LAGU LAGUA LAH ehhehhehe
+                    </div>
+                  </div>
+                </div>
+              </div>
+          </section>
+        </div>
+
+
+    </section><!-- End About Section -->
+         
+        <!-- Footer -->
+        <footer class="d-flex justify-content-center align-items-center mt-5">
+        <!-- Copyright -->
+            <div class="footer-copyright text-center py-3">© 2021 Copyright:
+             <a href="https://kripiklover.wordpress.com/"> Apen Jr</a>
+            </div>
+        <!-- Copyright -->  
+        </footer>
+        <!-- Footer -->
+  
+
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+
+  </body>
+</html>
